e0ae0233f57d617e3817eac0db5fe8085c7b0d47
VladislavPenchev <34232950+VladislavPenchev@users.noreply.github.com>
2020-04-26 13:58:02 +0300

    Update

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -2,26 +2,30 @@
 
 let gulp = require("gulp"),
 	autoprefixer = require("gulp-autoprefixer"),
-	// csso = require("gulp-csso"),
-	// size = require("gulp-size"),
+	csso = require("gulp-csso"),
+	size = require("gulp-size"),
 	browserSync = require('browser-sync').create(),
 	sass = require('gulp-sass'),
 	cp = require("child_process");
 
 gulp.task("sass", function() {
 	return gulp.src( '_scss/**/*.scss' )
-		// .pipe( size() )
+		.pipe( size() )
 		.pipe( sass().on('error', sass.logError) )
 		.pipe( autoprefixer() )
-		// .pipe( size() )
-		// .pipe( csso() )
-		// .pipe( size() )
+		.pipe( size() )
+		.pipe( csso() )
+		.pipe( size() )
 		.pipe( gulp.dest( './docs/css/' ) )
 		.pipe( browserSync.stream({ match: '**/*.css' }) )
 	;
 });
 
 // Jekyll
+gulp.task("jekyll-dev", function() {
+	return cp.spawn("bundle", ["exec", "jekyll", "build  --baseurl ''"], { stdio: "inherit", shell: true });
+});
+
 gulp.task("jekyll", function() {
 	return cp.spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit", shell: true });
 });
@@ -44,14 +48,15 @@ gulp.task("watch", function() {
 			"./_layouts/*.html",
 			"./_posts/**/*.*"
 		]
-	).on('change', gulp.series('jekyll', 'sass') );
+	).on('change', gulp.series('jekyll-dev', 'sass') );
 
 	gulp.watch( 'docs/**/*.html' ).on('change', browserSync.reload );
 	gulp.watch( 'docs/**/*.js' ).on('change', browserSync.reload );
 });
 
-gulp.task("default", gulp.series('jekyll', 'sass', 'watch'));
+gulp.task("default", gulp.series('jekyll-dev', 'sass', 'watch'));
 
-gulp.task("deploy", function() {
+gulp.task("deploy", gulp.series('jekyll', 'sass', function() {
 	return cp.spawn('git commit -am "Update" && git pull && git push', { stdio: "inherit", shell: true });
-});
+}));
+
